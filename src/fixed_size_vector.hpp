d770526910d2ea5a63a61c3ebb1e11cc90876769
Sanne <sanne-acc@posteo.net>
2017-01-09 23:39:13 +0100

    [new file] fixed_size_vector

---
+++ src/fixed_size_vector.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+// UTILS
+#include "container_io.hpp"
+
+// STL
+#include <vector>
+
+//--------------------------------------------------------------------------------------90
+/// @file fixed_size_vector.hpp
+/// @author Susanne van den Elsen
+/// @date 2016
+//----------------------------------------------------------------------------------------
+
+namespace datastructures
+{
+   //-------------------------------------------------------------------------------------
+   
+   // forward declarations
+   
+   template <typename T>
+   class fixed_size_vector;
+   
+   template <typename T>
+   std::ostream& operator <<(std::ostream& os, const fixed_size_vector<T>& vector);
+   
+   //-------------------------------------------------------------------------------------
+   
+   template <typename T>
+   class fixed_size_vector
+   {
+   public:
+      
+      using value_t = T;
+      
+      using vector_t = std::vector<value_t>;
+      
+      /// @brief Constructor
+      
+      explicit fixed_size_vector(std::size_t size, const value_t& value=value_t())
+      : m_vector(size, value)
+      , m_size(size)
+      {
+      }
+      
+      /// @brief Read-only subscript operator.
+      
+      const value_t& operator[](int index) const
+      {
+         return m_vector[index];
+      }
+      
+      /// @brief Subscript operator.
+      
+      value_t& operator[](int index)
+      {
+         return m_vector[index];
+      }
+      
+      /// @brief Returns the (constant) size of this fixed-size vector.
+      
+      std::size_t size() const
+      {
+         return m_size;
+      }
+      
+      typename vector_t::const_iterator begin() const
+      {
+         return m_vector.begin();
+      }
+      
+      typename vector_t::const_iterator end() const
+      {
+         return m_vector.end();
+      }
+      
+   private:
+      
+      vector_t m_vector;
+      
+      std::size_t m_size;
+      
+      friend std::ostream& operator << <>(std::ostream&, const fixed_size_vector<T>&);
+      
+   }; // end class template fixed_size_vector
+   
+   //-------------------------------------------------------------------------------------
+   
+   template <typename T>
+   std::ostream& operator << (std::ostream& os, const fixed_size_vector<T>& vector)
+   {
+      os << vector.m_vector;
+      return os;
+   }
+   
+   //-------------------------------------------------------------------------------------
+   
+} // end namespace datastructures
