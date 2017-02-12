3b6104ffb94768eafb2bb983bade92b00e6d4fae
Sanne <sanne-acc@posteo.net>
2017-02-12 11:53:45 +0100

    [feature] add macros DEBUG_SYNC serialized debug output

--- src/debug.hpp
+++ src/debug.hpp
@@ -1,28 +1,30 @@
+#pragma once
 
-#ifndef DEBUG_HPP_INCLUDED
-#define DEBUG_HPP_INCLUDED
-
+// STL
 #include <iostream>
+#include <thread>
 
-/*---------------------------------------------------------------------------75*/
-/**
- @file debug.hpp
- @brief Debugging macros.
- @author Susanne van den Elsen
- @date 2015
- */
-/*---------------------------------------------------------------------------++*/
+//--------------------------------------------------------------------------------------90
+/// @file debug.hpp
+/// @brief Debugging macros.
+/// @author Susanne van den Elsen
+/// @date 2015-2016
+//--------------------------------------------------------------------------------------90
 
+#define _DEBUG
 #ifdef _DEBUG
 #   define DEBUG(x) do { std::cout << x; } while (false)
 #   define DEBUGNL(x) do { std::cout << x << std::endl; } while (false)
 #   define DEBUGF(class, method, args, other) do { std::cout << class << "::" << method << "(" << args << ")\t" << other; } while (false)
 #   define DEBUGFNL(class, method, args, other) do { DEBUGF(class, method, args, other << std::endl); } while (false)
+static std::mutex debug_mutex;
+#   define DEBUG_SYNC(x) do { std::lock_guard<std::mutex> lock_guard(debug_mutex); DEBUG(x); } while (false)
+#   define DEBUGF_SYNC(class, method, args, other) do { std::lock_guard<std::mutex> lock_guard(debug_mutex); DEBUGF(class, method, args, other); } while (false)
 #else
 #   define DEBUG(x) do { } while (false)
 #   define DEBUGNL(x) do { } while (false)
 #   define DEBUGF(class, method, args, other) do { } while (false)
 #   define DEBUGFNL(class, method, args, other) do { } while (false)
-#endif
-
+#   define DEBUG_SYNC(x) do { } while (false)
+#   define DEBUGF_SYNC(class, method, args, other) do { } while (false)
 #endif
